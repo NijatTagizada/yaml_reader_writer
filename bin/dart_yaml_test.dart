955cd90e45ce75571da05a0034de745eb8f4b635
NijatTagizada <tagizada.nicat@gmail.com>
2021-10-24 16:33:08 +0400

    init commit

--- bin/dart_yaml_test.dart
+++ bin/dart_yaml_test.dart
@@ -5,15 +5,22 @@ import 'package:yaml/yaml.dart';
 import 'yaml_writer.dart';
 
 void main(List<String> arguments) async {
+  // Get File
   var yamlFile = File('pubspec.yaml');
+
+  // Convert to YamlMap
   String yamlStr = await yamlFile.readAsString();
   YamlMap yamlMap = loadYaml(yamlStr);
+
+  // Convert to Map
   Map map = yamlMap.toMap();
 
+  // Dependencies section
   Map dependenciesMap = map['dependencies'];
   dependenciesMap['dio'] = '^4.0.1';
   dependenciesMap['http'] = '^0.13.4';
 
+  // Write map in yaml
   final writer = YamlWriter();
   String writeString = writer.write(map);
   yamlFile.writeAsStringSync(writeString, mode: FileMode.write);
